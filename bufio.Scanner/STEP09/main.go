32a33d8df9d6f9764990e1d6464a2250ca7a374b
tmpbook <nauy2011@126.com>
2018-06-08 14:13:00 +0800

    Protecting against endless loop

--- bufio.Scanner/STEP09/main.go
+++ bufio.Scanner/STEP09/main.go
@@ -11,6 +11,10 @@ func main() {
 	input := "foo|bar"
 	scanner := bufio.NewScanner(strings.NewReader(input))
 	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		// 防止无限循环的方法
+		// if atEOF && len(data) == 0 {
+		// 	return 0, nil, nil
+		// }
 		if i := bytes.IndexByte(data, '|'); i >= 0 {
 			return i + 1, data[0:i], nil
 		}
